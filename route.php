3edd94a76e14239238bdda8f31ffd6e46b2d13dd
Ailin Kravos <ailinkravos@gmail.com>
2019-11-04 20:49:19 -0300

    agrego la carpeta libs y modificacion del route

--- route.php
+++ route.php
@@ -1,5 +1,5 @@
 <?php
-require_once ('controller/FacultadesController.php');
+require_once ('controllers/FacultadesController.php');
 require_once ('controllers/LoginController.php');
 
 define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
@@ -8,14 +8,14 @@ define("LOGIN", BASE_URL . 'login');
 define("URL_OLIMPIADAS", BASE_URL . 'olimpiadas');
 
 $action = $_GET['action'];//tomo el valor del action (accion que haga el usuario)
-$genderController = new GenderController();
+$facultadesController = new FacultadesController();
 // $adminController = new AdminController();
 $controllerLogin = new LoginController();
 //hago un objeto de la class GenderController
-$serieController = new SerieController();
+// $serieController = new SerieController();
     
     if($action==''){//si action es nulo se muestra el index con la series
-        $genderController -> showIndex();
+        $facultadesController -> showIndex();
         // $genderController -> getGenders();
     }
     elseif(isset($action)){
@@ -24,53 +24,53 @@ $serieController = new SerieController();
             if($url[0] == 'login'){
                 $controllerLogin -> showLogin();
             }
-            if(isset($url[1]) && $url[1] == 'add'){
-                $genderController -> addGender();
-                die();
-            }
-            if(isset($url[1]) && $url[1] == 'edit'){
-                $genderController -> editGender($url[2]);
-                die();
-            }
-            if(isset($url[1]) && $url[1] == 'delete'){
-                $genderController -> deleteGender($url[2]);
-                die();
-            }
-            if(isset($url[1]) && $url[1] == 'addSerie'){
-                $serieController -> addSerie();
-                die();
-            }
-            if(isset($url[1]) && $url[1] == 'editSerie'){
-                $serieController -> editSerie();
-                die();
-            }
-            if(isset($url[1]) && $url[1] == 'deleteSerie'){
-                $serieController -> deleteSerie();
-                die();
-            }
-            if($url[0] == 'enterSession'){
-                // $controllerLogin -> verifyUser();
-                $genderController -> showIndexAdmin();
-            }
-            if($url[0] == 'verifyLog'){
-                $controllerLogin -> verifyUser();
-                $genderController -> showIndexAdmin();
-            }
-            if($url[0] == 'logout'){
-                $controller = new LoginController();
-                $controller -> logout();
-            }
-            // ------agregar/editar/borrar generos ----
-            if($url[0]=='insertGender'){
-                $genderController->addGender();
-            }
-            if($url[0]=='serie'){
-                $infoSerie = $serieController->getSerie($url[1]);
-                $genderName = $genderController -> getGenderByID($infoSerie);
-                $serieController->showSerie($infoSerie, $genderName);
-            }  
-            if($url[0]=='genero'){
-                $gender = $genderController->getGender($url[1]);
-                $serieController->getSeriesOfGender($gender);
-            }
+            // if(isset($url[1]) && $url[1] == 'add'){
+            //     $genderController -> addGender();
+            //     die();
+            // }
+            // if(isset($url[1]) && $url[1] == 'edit'){
+            //     $genderController -> editGender($url[2]);
+            //     die();
+            // }
+            // if(isset($url[1]) && $url[1] == 'delete'){
+            //     $genderController -> deleteGender($url[2]);
+            //     die();
+            // }
+            // if(isset($url[1]) && $url[1] == 'addSerie'){
+            //     $serieController -> addSerie();
+            //     die();
+            // }
+            // if(isset($url[1]) && $url[1] == 'editSerie'){
+            //     $serieController -> editSerie();
+            //     die();
+            // }
+            // if(isset($url[1]) && $url[1] == 'deleteSerie'){
+            //     $serieController -> deleteSerie();
+            //     die();
+            // }
+            // if($url[0] == 'enterSession'){
+            //     // $controllerLogin -> verifyUser();
+            //     $genderController -> showIndexAdmin();
+            // }
+            // if($url[0] == 'verifyLog'){
+            //     $controllerLogin -> verifyUser();
+            //     $genderController -> showIndexAdmin();
+            // }
+            // if($url[0] == 'logout'){
+            //     $controller = new LoginController();
+            //     $controller -> logout();
+            // }
+            // // ------agregar/editar/borrar generos ----
+            // if($url[0]=='insertGender'){
+            //     $genderController->addGender();
+            // }
+            // if($url[0]=='serie'){
+            //     $infoSerie = $serieController->getSerie($url[1]);
+            //     $genderName = $genderController -> getGenderByID($infoSerie);
+            //     $serieController->showSerie($infoSerie, $genderName);
+            // }  
+            // if($url[0]=='genero'){
+            //     $gender = $genderController->getGender($url[1]);
+            //     $serieController->getSeriesOfGender($gender);
+            // }
     }
