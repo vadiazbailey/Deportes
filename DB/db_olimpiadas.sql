c44fc7d5dd950ee7909c36967791377cb1c45bc4
Ayelen Diaz Bailey <vdiazbailey@alumnos.exa.unicen>
2019-11-07 17:04:43 -0300

    actualizo bbdd

--- DB/db_olimpiadas.sql
+++ DB/db_olimpiadas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-11-2019 a las 00:34:23
--- Versión del servidor: 10.4.6-MariaDB
--- Versión de PHP: 7.3.8
+-- Tiempo de generación: 07-11-2019 a las 21:03:47
+-- Versión del servidor: 10.1.26-MariaDB
+-- Versión de PHP: 7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,6 +25,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `alumno`
+--
+
+CREATE TABLE `alumno` (
+  `id_alumno` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `DNI` int(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `celular` int(50) NOT NULL,
+  `id_facultad_fk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `alumno`
+--
+
+INSERT INTO `alumno` (`id_alumno`, `nombre`, `apellido`, `DNI`, `email`, `celular`, `id_facultad_fk`) VALUES
+(1, 'Viviana Ayelén', 'Diaz Bailey', 37926567, 'ayediazbailey@gmail.com', 2494555, 4);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `facultad`
 --
 
@@ -40,11 +63,29 @@ CREATE TABLE `facultad` (
 --
 
 INSERT INTO `facultad` (`id_facultad`, `nombre_facultad`, `sede`, `historia`) VALUES
-(1, 'Agronomía', 'Azul', 'Por el momento no tienen una mascota definida.\r\n\r\nLos colores que utilizan son el bordó y el blanco.');
+(1, 'Agronomía', 'Azul', 'Por el momento no tienen una mascota definida.\r\n\r\nLos colores que utilizan son el bordó y el blanco.'),
+(2, 'Arte', 'Tandil', 'Adoptaron como mascota al arlequín.\r\n\r\nSus colores varían entre las tonalidades del rojo. Sus camisetas suelen ser color azul violáceo y rojo.'),
+(3, 'Derecho', 'Azul', 'Esta facultad si bien no tiene del todo definida su mascota, el logo de las remeras de sus equipos es un cuervo. Los jugadores decidieron poner esta mascota porque a los abogados se los suele identificar con ese animal.\r\n\r\nLos colores con lo que los vas a poder identificar son el magenta y el azul.'),
+(4, 'Económicas', 'Tandil', 'Según cuenta la leyenda, los actuales contadores Gabriel Dadiego y Mario Cabitto fueron designados para establecer el apodo del Centro de estudiantes y los colores que se usarían en olimpiadas.\r\n\r\nEllos, hinchas de Estudiantes de La Plata uno y de Racing Club el otro, hicieron ostentación de esa condición para elegir el apodo de \"León\" y los colores celeste y blanco.'),
+(5, 'Exactas', '', 'Según el boca en boca, la facultad de Exactas es representada por el gallo debido a que hace mucho tiempo, veterinarias hizo una canción llamándoles gallinas y ellos les retrucaron con otra canción autoproclamándose gallos, alegando a la fiereza combativa de dicho animal.\r\n\r\nEn cuanto a los colores, la historia se remonta a las primeras olimpiadas donde no existía ningún tipo de presupuesto para las remeras. Ante esto, el Club Santamarina les prestó remeras suyas de color oro y negro.'),
+(6, 'Humanas', 'Tandil', 'Su mascota es Boxitracio, un personaje inventado por García Ferré para la serie Hijitus. El prefijo “Boxi” anticipa de alguna manera la condición de boxeador del animal.\r\n\r\nSus colores son el azul y el oro.'),
+(7, 'Ingeniería', 'Olavarría', 'En el año 2016 se largó una encuesta por Facebook, para que los alumnos puedan decidir la misma, en donde la mascota que obtuvo más votos fue un Lobo.\r\n\r\nLos colores con los cuales lo vamos a encontrar son verde oscuro y blanco; éstos pueden ser combinados con azul en ciertas ocasiones, ya que este color forma parte del logo del centro de estudiantes.'),
+(8, 'Quequén', 'Quequén', 'El origen de su mascota es reciente, la misma es un duende. La historia del porqué se eligió esta mascota viene dada porque en los alrededores del predio, donde se encuentra la sede, está rodeado de gnomos.\r\n\r\nTomando de esta misma manera sus colores, un azul marino, medio verdoso, por el mar.'),
+(9, 'Salud', 'Olavarría', 'Se identifican con los colores verde y azul.\r\n\r\nNo tienen mascota por el momento.'),
+(10, 'Sociales', 'Olavarría', 'Sus colores son el naranja y el negro.\r\n\r\nPor el momento no tienen definida una mascota.'),
+(11, 'Veterinarias', 'Tandil', 'Son conocidos como los borrachos del tablón, no tienen una mascota bien definida, pero el resto de las facultades lo representan con un paisano. Por los equipos de futsal femenino y básquet se vienen representando con el chancho.\r\n\r\nSus colores vienen definidos porque mundialmente la cruz violeta es representativa de la veterinaria, junto con el blanco y negro.'),
+(20, 'ailu', 'pedro', 'ailu');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  ADD PRIMARY KEY (`id_alumno`),
+  ADD UNIQUE KEY `id_facultad_fk` (`id_facultad_fk`);
 
 --
 -- Indices de la tabla `facultad`
@@ -57,10 +98,24 @@ ALTER TABLE `facultad`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  MODIFY `id_alumno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT de la tabla `facultad`
 --
 ALTER TABLE `facultad`
-  MODIFY `id_facultad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_facultad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  ADD CONSTRAINT `alumno_ibfk_1` FOREIGN KEY (`id_facultad_fk`) REFERENCES `facultad` (`id_facultad`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
